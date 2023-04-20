0f7de95d7ff7ce4234b3cbc00790a34c4de556bf
tobias <tobirastorza@gmail.com>
2023-04-20 00:03:43 -0300

    20 apr connected to mongo

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,42 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const Person = require('./models/person')
 
 app.use(express.json());
-
-morgan.token("data", (request, response) => {
-  return request.method === "POST" ? JSON.stringify(request.body) : " ";
-});
 
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+    Person.find({}).then((person) => {
+      console.log(person);
+    });
 });
 
 app.get("/info", (request, response) => {
@@ -95,7 +72,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
